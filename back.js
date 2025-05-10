ed763158a02592f33a4577301f488792d370fc0b
TIAGO CESAR DA SILVA <05692445441@edu.fb.org.br>
2025-05-09 21:38:33 -0300

    Links dinamicos

--- back.js
+++
@@ -1,5 +0,0 @@
-console.log ('Resultado 1', (10 > 5) && (5 > 1));
-console.log ('Resultado 2', (4 == 4) && (10 < 5));
-console.log ('Resultado 3', (3 >= 2) && (2 <= 2));
-console.log ('Resultado 4', (7 != 7) && (5 > 4));
-console.log ('Resultado 5', (8 > 3) && (3 > 10));
